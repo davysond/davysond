baf00b3be68d9d01985e9e9e498c760fa03f7cfc
Davyson D <61214483+davysond@users.noreply.github.com>
2022-09-26 11:41:48 -0300

    Add files via upload
    Inserindo os arquivos iniciais.

---
+++ insert_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO USERS(id, first_name, last_name, course, university) VALUES (1, "Davyson", "Guimarães", "Ciência da Computação", "UFCG");
+INSERT INTO USERS(id, first_name, last_name, course, university) VALUES (1, "Jackson", "Guimarães", "Geografia", "UEPB");
+INSERT INTO USERS(id, first_name, last_name, course, university) VALUES (1, "Maria", "Guimarães", "Pedagogia", "UEPB");
